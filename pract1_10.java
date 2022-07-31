34336fc6acc31547ee7213271b0411f1fb8e566c
khushigoriya <109236374+khushigoriya@users.noreply.github.com>
2022-07-31 18:03:09 +0530

    Add files via upload

---
+++ pract1_10.java
@@ -0,0 +1,66 @@
+//ID: 21CE035
+//Name: KHUSHI GORIYA
+//Aim: Implement Caesar Cipher.
+import java.util.*;
+public class pract1_10
+{  
+    public static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+ 
+    public static String Encryption(String PlainText, int ShiftKey)
+    {
+        PlainText = PlainText.toLowerCase();
+        
+        String CipherText = "";
+        
+        for (int i = 0; i < PlainText.length(); i++)
+        {
+            int CharPosition = ALPHABET.indexOf(PlainText.charAt(i));
+            int KeyValue = (ShiftKey + CharPosition) % 26;
+            char ReplaceValue = ALPHABET.charAt(KeyValue);
+            CipherText += ReplaceValue;
+        }
+        
+        return CipherText;
+    }
+ 
+    public static String Decryption(String CipherText, int ShiftKey)
+    {
+        CipherText = CipherText.toLowerCase();
+        
+        String PlainText = "";
+        
+        for (int i = 0; i < CipherText.length(); i++)
+        {
+            int CharPosition = ALPHABET.indexOf(CipherText.charAt(i));
+            int KeyValue = (CharPosition - ShiftKey) % 26;
+            
+            if (KeyValue < 0)
+            {
+                KeyValue = ALPHABET.length() + KeyValue;
+            }
+            
+            char ReplaceValue = ALPHABET.charAt(KeyValue);
+            
+            PlainText += ReplaceValue;
+        }
+        
+        return PlainText;
+    }
+ 
+    public static void main(String[] args)
+    {
+        Scanner s = new Scanner(System.in);
+        
+        System.out.print("Enter String for Encryption : ");
+        
+        String Message = new String();
+        Message = s.next();
+        
+        System.out.print("Encrypted String : ");
+        System.out.println(Encryption(Message, 3));
+        
+        System.out.print("Decrypted String : ");
+        System.out.println(Decryption(Encryption(Message, 3), 3));
+        s.close();
+    }
+}
