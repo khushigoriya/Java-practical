81bbdf2a460a2c5c58ef90cb6d6c55f9ec59df29
khushigoriya <109236374+khushigoriya@users.noreply.github.com>
2022-07-31 17:30:54 +0530

    Update pract1_5.java

--- pract1_5.java
+++ pract1_5.java
@@ -1,3 +1,7 @@
+//NAME: KHUSHI GORIYA 
+// ID: 21CE035
+/* AIM:Given 2 strings, a and b, return the number of the positions where they contain the same length 2 substring. So "xxcaazz" and "xxbaaz" yields 3, since the "xx", "aa", and "az" substrings appear in the same place in both strings.*/
+
 import java.util.*;
 public class pract1_5{
     static public int stringMatch(String a, String b) {
