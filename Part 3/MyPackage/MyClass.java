cce5a79f392c186a8034c70e68b878d239367b7c
khushigoriya <109236374+khushigoriya@users.noreply.github.com>
2022-09-15 17:04:34 +0530

    Add files via upload

---
+++ Part 3/MyPackage/MyClass.java
@@ -0,0 +1,12 @@
+package MyPackage;
+
+public class MyClass {
+    public int sum(int a,int b)
+    {
+        return a+b;
+    }
+    public int diff(int a,int b)
+    {
+        return a-b;
+    }    
+}
